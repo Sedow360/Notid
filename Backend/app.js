40ae14d7ec335f65e73a232be16ce60726020a39
Sedow360 <ayushmaiti2004@gmail.com>
2026-01-03 13:30:35 +0530

    .

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-    origin: "https://notid-frontend-r54xbefxs-ayushs-projects-4645ae36.vercel.app",
+    origin: "https://notid-frontend.vercel.app/",
     credentials: true
 }));
 
